4142f0a16a409d883850884fc821f665a3d94a90
Katelynn Lauricello <>
2021-04-22 14:19:06 +0000

    DOne

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
      Scanner scan = new Scanner(System.in);
@@ -9,7 +10,7 @@ class Main {
     {
         System.out.println("As you explore the spaceship you find a datapad.\n What should you do? \n Hack it open (H) \n Leave it b (B)");
          String datapad = scan.next();
-        if (datapadGenerator.equals("B")) 
+        if (datapadGenerator.equals("H")) 
         {
             int datapadGenerator = (int) (Math.random() * 2);
             if (datapadGenerator == 1) 
@@ -18,10 +19,16 @@ class Main {
           }else{
           System.out.println("Aw shucks sorry but you failed");
             }
-        }else{
-          System.out.println("You continue around the ship till you find a door seeming leading to outside \n You kick it down \n The blazing hot desert air hit you like the chancla from your mother. \n no escpae in sight you might be here a while. \n End of Adventure  ");
-        }
-
-    }
+          if (datapadGenerator.equals("B"))
+          {
+          System.out.println("You continue around the ship till you find a door seeming leading to outside \n You kick it down \n The blazing hot desert air hit you like the chancla from your mother. \n no escpae in sight you might be here a while. \n End of Adventure");
+          }
+   }
+  if (spaceship.equals("B"))
+  {
+    System.out.println("You come across an old pipe. \n great for wacking people with \n You continue around the ship till you find a door seeming leading to outside \n You kick it down \n The blazing hot desert air hit you like the chancla from your mother. \n no escpae in sight you might be here a while. \n End of Adventure");
   }
+    
+  }
+}
 }
